9f3ab412b2616305bc852f6e62023b29c0264d76
Tzvi-H <16guitar@gmail.com>
2019-12-09 12:19:43 +0800

    completed conditionals

---
+++ conditionals/logical_conditions_3.js
@@ -0,0 +1,6 @@
+// Without running the below code, determine what will be logged to the console.
+
+let sale = true;
+let admissionPrice = !sale ? 5.25 : 3.99;
+
+console.log('$' + admissionPrice);
---
+++ conditionals/truthy_vs_falsy.js
@@ -0,0 +1,6 @@
+// Without looking at your notes or MDN, try to recall all 
+// values that count as falsy in JavaScript.
+
+/*
+'', 0, false, undefined, null, NaN
+*/
